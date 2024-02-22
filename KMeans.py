36c32185c314f29453221e392ecf881b1395a80b
ruchamaslekar <140859360+ruchamaslekar@users.noreply.github.com>
2024-02-21 19:04:32 -0800

    modifed kemean.py file

--- KMeans.py
+++ KMeans.py
@@ -1,8 +1,7 @@
- kmeans.py
-from cluster import Cluster
+from cluster import cluster
 import numpy as np
 
-class KMeans(Cluster):
+class KMeans(cluster):
     def __init__(self, k=5, max_iterations=100):
         super().__init__()
         self.k = k
@@ -11,19 +10,20 @@ class KMeans(Cluster):
     def fit(self, X):
         X = np.array(X)
 
-        # Initialize centroids randomly
+        # Initializing the centroids randomly
         centroids = X[np.random.choice(X.shape[0], self.k, replace=False)]
 
         for _ in range(self.max_iterations):
-            # Assign each instance to the closest centroid
+            # Assigning each instance to the closest centroid
             labels = np.argmin(np.linalg.norm(X[:, np.newaxis] - centroids, axis=2), axis=1)
 
-            # Update centroids based on mean of assigned instances
+            # Updating centroids based on mean of assigned instances
             new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.k)])
 
-            # Check for convergence
+            # Checking for convergence
             if np.all(centroids == new_centroids):
                 break
 
             centroids = new_centroids
-           return labels.tolist(), centroids.tolist())
+
+        return labels.tolist(), centroids.tolist()
